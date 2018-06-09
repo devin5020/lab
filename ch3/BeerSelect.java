dd516314a73ea728fa309759786b072c8b215de5
devin5020 <omtfzz@gmail.com>
2018-06-09 23:14:46 +0800

    servlet1
    设计提交页面，但未有mvc的m逻辑

---
+++ ch3/BeerSelect.java
@@ -0,0 +1,19 @@
+
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class BeerSelect extends HttpServlet{
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+                       throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Beer Selection Advice<br>");
+        String c= request.getParameter("color");
+        out.println("<br>Got Beer color "+c);
+
+    }
+
+}
