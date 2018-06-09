edfb96783437f648db6b51ce801104906bd7a1bc
devin5020 <omtfzz@gmail.com>
2018-06-09 23:31:35 +0800

    Add files via upload
    add Model Logic 
    beerselect component invoke the beerexpert component

--- ch3/BeerSelect.java
+++ ch3/BeerSelect.java
@@ -3,16 +3,26 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
 
 public class BeerSelect extends HttpServlet{
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response)
                        throws IOException, ServletException {
+        String c= request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+
+
+        
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("Beer Selection Advice<br>");
-        String c= request.getParameter("color");
-        out.println("<br>Got Beer color "+c);
+        
+        Iterator it = result.iterator();
+        while(it.hasNext()) {
+            out.print("<br>try: " + it.next());
+        }
 
     }
 
